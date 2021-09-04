6a298e09f32a789c746ac3a00529adad52831930
BaxterDulieu <69779295+BaxterJDulieu@users.noreply.github.com>
2021-09-04 13:16:09 +1000

    WallOutlinePrefab

--- Game/AdaptiveGameJam v2/Assets/Scripts/ScoreManager.cs
+++ Game/AdaptiveGameJam v2/Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void IncreaseScore()
+    {
+
     }
 }
