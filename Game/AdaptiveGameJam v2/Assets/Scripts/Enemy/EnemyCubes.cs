e317c7fc66b7096f77a369eb48462f816bba4c9d
BaxterDulieu <69779295+BaxterJDulieu@users.noreply.github.com>
2021-09-04 14:57:03 +1000

    Collisions 3/4 done

---
+++ Game/AdaptiveGameJam v2/Assets/Scripts/Enemy/EnemyCubes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCubes : MonoBehaviour
+{
+    SpawnGrid spawnGrid;
+    bool hasHit = false;
+    void Start()
+    {
+        GameObject gameManager = GameObject.FindGameObjectWithTag("GameController");
+        spawnGrid = gameManager.GetComponent<SpawnGrid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < 0 && hasHit == false && gameObject.tag == "Vacant")
+        {
+            print("Missed"); //Chris implement something here for health
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            hasHit = true;
+        }
+    }
+}
