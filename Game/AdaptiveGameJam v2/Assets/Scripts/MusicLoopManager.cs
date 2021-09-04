3033dc1cf4f8e088afcc3c2695c36d152c36c49a
ItsFitzy <79495553+ItsFitzy@users.noreply.github.com>
2021-09-04 22:02:42 +1000

    Music System Working
    - Added Music Player prefab controlled by MusicLoopManager, which is created in Main Menu, and set to Don't Destroy On Load
    - Added in the Endless Runner music track

---
+++ Game/AdaptiveGameJam v2/Assets/Scripts/MusicLoopManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoopManager : MonoBehaviour
+{
+    //Properties
+    [SerializeField] float loopStartTime; //time (in seconds) into the track that should be looped back to
+    [SerializeField] float loopEndTime; //time (in seconds) into the track that the loop should be created
+
+
+    //Variables
+    GameObject oldPlayer, newPlayer;
+    float trackLength;
+
+
+    //References
+    public GameObject musicPlayerPrefab;
+
+
+    //Engine-called
+    void Start(){
+        DontDestroyOnLoad(gameObject);
+
+        newPlayer = Instantiate(musicPlayerPrefab, transform);
+        trackLength = newPlayer.GetComponent<AudioSource>().clip.length;
+        Invoke("LoopTrack", loopEndTime);
+    }
+
+
+    //Methods
+    void LoopTrack(){
+        oldPlayer = newPlayer;
+        newPlayer = Instantiate(musicPlayerPrefab, transform);
+        newPlayer.GetComponent<AudioSource>().time = loopStartTime;
+        Invoke("DestroyOldPlayer", trackLength - loopEndTime);
+        Invoke("LoopTrack", loopEndTime - loopStartTime);
+    }
+
+    void DestroyOldPlayer(){
+        Destroy(oldPlayer);
+    }
+}
