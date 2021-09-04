62cd45de189309ee707d6354f62b263b53004dc0
BaxterDulieu <69779295+BaxterJDulieu@users.noreply.github.com>
2021-09-04 20:41:56 +1000

    Changed Player Material

--- Game/AdaptiveGameJam v2/Assets/Scripts/PlayerCubes.cs
+++ Game/AdaptiveGameJam v2/Assets/Scripts/PlayerCubes.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class PlayerCubes : MonoBehaviour
 {
     PlayerManager playerManager;
+    public Material gridMaterial, transparentMaterial;
 
     // Start is called before the first frame update
+    
     void Start()
     {
        
         GameObject manager = GameObject.FindGameObjectWithTag("GameController");
         playerManager = manager.GetComponent<PlayerManager>();
+        
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Transparent"))
+        {
+            gameObject.GetComponent<MeshRenderer>().material = transparentMaterial;
+        }
+        if (Input.GetButtonUp("Transparent"))
+        {
+            gameObject.GetComponent<MeshRenderer>().material = gridMaterial;
+        }
+        if (playerManager.gameOver)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Enemy")
