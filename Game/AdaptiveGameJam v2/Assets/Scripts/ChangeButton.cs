8529a6007b84c6a66e219cdf770881b92cb610ed
BaxterDulieu <69779295+BaxterJDulieu@users.noreply.github.com>
2021-09-05 17:57:22 +1000

    Implemented New Grid Sprites

---
+++ Game/AdaptiveGameJam v2/Assets/Scripts/ChangeButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeButton : MonoBehaviour
+{
+
+    private Image spriteRenderer;
+    public Sprite blueSprite, redSprite;
+    private GamemodeManager gamemodeManager;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<Image>();
+        gamemodeManager = FindObjectOfType<GamemodeManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gamemodeManager.currentGameMode == GamemodeManager.GameMode.Adapt)
+        {
+            spriteRenderer.sprite = blueSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = redSprite;
+        }
+    }
+}
